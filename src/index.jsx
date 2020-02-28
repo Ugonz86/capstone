4c4502a7a64c0174497e77352b108d27239de28e
Ugonz86 <ugonzalez86@gmail.com>
2020-02-28 11:42:51 -0800

    Add webpack and babel

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>ebills</h1>
+    <p><em>Firebase will not save record!</em></p>
+  </div>,
+  document.getElementById('capstone')
+);
