72f39029fa9d21045bbfc529b5020594a1287ab8
Ugonz86 <ugonzalez86@gmail.com>
2020-02-28 12:25:25 -0800

    Add webpack server and hot module replacement

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('capstone')
-);
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    document.getElementById('capstone')
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App)
+  });
+}
